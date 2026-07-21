0067eee733297f7acbdcf67e6759905a1fc75624
vancebol <arkhandika.bw@gmail.com>
2026-07-21 15:44:08 +0700

    feat: update CV download link in Navbar component to new file

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -74,7 +74,7 @@ export function Navbar() {
 
           <div className="hidden md:block">
             <Button size="sm" variant="secondary" asChild>
-              <a href="/resume.pdf" download aria-label="Download CV">
+              <a href="/CV_Arkhandika Budi W.pdf" download aria-label="Download CV">
                 <Download className="h-4 w-4" />
                 Download CV
               </a>
